f20ce167227f3d75964e6b5489f6a05531752523
Aiman <aymen123mengesha@gmail.com>
2022-04-08 10:58:00 -0700

    C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - block
+ * discription: prints in lowwercase then newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
